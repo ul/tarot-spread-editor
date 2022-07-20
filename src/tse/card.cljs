9e42bfab46034aae358c3fcca14eedb29b96c349
Ruslan Prakapchuk <fer.obbee@gmail.com>
2022-07-21 08:33:11 +1000

    fix Futhork aspect ratio

--- src/tse/card.cljs
+++ src/tse/card.cljs
@@ -5,12 +5,18 @@
             [tse.transformer :as trans]))
 
 (defn view [ctx id]
-  (let [ref (trans/make-interact ctx (atom nil))]
+  (let [ref (trans/make-interact ctx (atom nil))
+        ratio (rx/cell nil)]
     (fn [{:keys [sub emit] :as ctx} id]
       (let [{:keys [src angle z-index]
              [x y] :origin
              [w h] :dimensions}
             @(sub [:card/entity id])
+            offset-x
+            (if @ratio (* 0.5 (- w (* h @ratio))) 0.0)
+            on-load
+            (fn [e]
+              (reset! ratio (/ (.. e -target -width) (.. e -target -height))))
             on-mousedown
             (fn [e]
               (.stopPropagation e)
@@ -20,11 +26,11 @@
           :ref ref
           :style
           {:position "absolute"
-           :transform (str/format "translate(%spx, %spx) rotate(%srad)" x y angle)
+           :transform (str/format "translate(%spx, %spx) rotate(%srad)" (+ x offset-x) y angle)
            :will-change "transform"
            :zIndex z-index
-           :width w
            :height h}
+          :on-load on-load
           ;; NOTE mousedown is not fired in Firefox and pointerdown in Safari for somewhat reason
           :on-mousedown (when-not (browser/isFirefox) on-mousedown)
           :on-pointerdown (when (browser/isFirefox) on-mousedown)}]))))
