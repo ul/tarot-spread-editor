bf59ae100ef482c283e7ba9d6401026311766f90
Ruslan Prakapchuk <fer.obbee@gmail.com>
2026-01-07 16:14:32 +1100

    add shift mode button for touch devices

--- src/tse/toolbar.cljs
+++ src/tse/toolbar.cljs
@@ -2,17 +2,25 @@
   (:require))
 
 (defn view [{:keys [sub emit]}]
-  [:div
-   {:style {:display "flex"
-            :margin "1em"}}
-   ;; [:div {:style {:flex 1}}]
-   [:button.pure-button.button-destructive
-    {:disabled (when (empty? @(sub [:item/selected])) "disabled")
-     :title @(sub [:t :toolbar/remove-selected])
-     :on-click #(emit [:item/remove-selected])}
-    [:i.fa.fa-times]]
-   [:button.pure-button.button-destructive
-    {:disabled (when (empty? @(sub [:item/all])) "disabled")
-     :title @(sub [:t :toolbar/remove-all])
-     :on-click #(emit [:item/remove-all])}
-    [:i.fa.fa-trash]]])
+  (let [shift-mode? @(sub [:transformer/shift-mode?])
+        has-selected? (not (empty? @(sub [:item/selected])))]
+    [:div
+     {:style {:display "flex"
+              :margin "1em"}}
+     [:button.pure-button.button-destructive
+      {:disabled (when-not has-selected? "disabled")
+       :title @(sub [:t :toolbar/remove-selected])
+       :on-click #(emit [:item/remove-selected])}
+      [:i.fa.fa-times]]
+     [:button.pure-button.button-destructive
+      {:disabled (when (empty? @(sub [:item/all])) "disabled")
+       :title @(sub [:t :toolbar/remove-all])
+       :on-click #(emit [:item/remove-all])}
+      [:i.fa.fa-trash]]
+     [:div {:style {:flex 1}}]
+     [:button.pure-button
+      {:disabled (when-not has-selected? "disabled")
+       :title "Toggle resize mode"
+       :style {:background-color (when shift-mode? "rgba(100,149,237,0.9)")}
+       :on-click #(emit [:transformer/shift-mode (not shift-mode?)])}
+      [:i.fa.fa-expand]]]))
