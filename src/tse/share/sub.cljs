f59a880bc2f8114f2f0510f5ee8e937dfe3379ff
Ruslan Prakapchuk <fer.obbee@gmail.com>
2026-01-08 18:56:06 +1100

    debounce state serialization

--- src/tse/share/sub.cljs
+++ src/tse/share/sub.cljs
@@ -6,15 +6,24 @@
 
 (def serialize-keys [:active-deck :active-suit :canvas :grid :background :lang :items])
 
+(def ^:private debounce-timeout (atom nil))
+
+(defn- write-fragment! [state]
+  (let [hash (->> state (t/write w) js/encodeURIComponent)
+        loc js/window.location
+        uri (str (.-pathname loc) (.-search loc) "#" hash)]
+    (js/history.replaceState nil nil uri)))
+
 (defn update-fragment [{:keys [db sub]}]
   (let [state (rx/rx (select-keys @db serialize-keys))
         loaded? (rx/rx (-> @db :loaded?))
         dragging? (sub [:transformer/dragging?])]
     (rx/rx
      (when (and @loaded? (not @dragging?))
-       (let [hash (->> @state (t/write w) js/encodeURIComponent)
-             loc js/window.location
-             uri (str (.-pathname loc) (.-search loc) "#" hash)]
-         (js/history.replaceState nil nil uri))))))
+       (let [current-state @state]
+         (when-let [t @debounce-timeout]
+           (js/clearTimeout t))
+         (reset! debounce-timeout
+                 (js/setTimeout #(write-fragment! current-state) 1000)))))))
 
 (def spec {:share/update-fragment update-fragment})
