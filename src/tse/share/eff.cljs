0535ba9307a1b335906cab7535e9caa54c3ce44b
Ruslan Prakapchuk <fer.obbee@gmail.com>
2026-01-24 17:35:28 +1100

    fix history handling

--- src/tse/share/eff.cljs
+++ src/tse/share/eff.cljs
@@ -1,13 +1,17 @@
 (ns tse.share.eff
-  (:require [cognitect.transit :as t]))
+  (:require [cognitect.transit :as t]
+            [carbon.rx :as rx :include-macros true]
+            [tse.share.sub :refer [*navigating?]]))
 
 (def r (t/reader :json))
 
 (defn load-from-fragment [{:keys [db]}]
+  (reset! *navigating? true)
   (let [loc js/window.location
         hash (-> (.-hash loc) (subs 1) (js/decodeURIComponent))]
-    (when (pos? (count hash))
-      (swap! db merge (t/read r hash)))
-    (swap! db assoc :loaded? true)))
+    (rx/dosync
+     (when (pos? (count hash))
+       (swap! db merge (t/read r hash)))
+     (swap! db assoc :loaded? true))))
 
 (def spec {:share/load-from-fragment load-from-fragment})
--- src/tse/share/sub.cljs
+++ src/tse/share/sub.cljs
@@ -2,28 +2,31 @@
   (:require [carbon.rx :as rx :include-macros true]
             [cognitect.transit :as t]))
 
+(def *navigating? (atom false))
+
 (def w (t/writer :json))
 
-(def serialize-keys [:active-deck :active-suit :canvas :grid :background :lang :items])
+(def serialize-keys
+  [:active-deck :active-suit :canvas :grid :background :lang :items])
 
-(def ^:private debounce-timeout (atom nil))
+(def ^:private *debounce-timeout (atom nil))
 
-(defn- write-fragment! [state]
-  (let [hash (->> state (t/write w) js/encodeURIComponent)
-        loc js/window.location
-        uri (str (.-pathname loc) (.-search loc) "#" hash)]
-    (js/history.pushState nil nil uri)))
+(defn- write-fragment!
+  [state]
+  (let [loc js/window.location]
+    (->> state
+         (t/write w)
+         js/encodeURIComponent
+         (str (.-pathname loc) (.-search loc) "#")
+         (js/history.pushState nil nil))))
 
-(defn update-fragment [{:keys [db sub]}]
-  (let [state (rx/rx (select-keys @db serialize-keys))
-        loaded? (rx/rx (-> @db :loaded?))
-        dragging? (sub [:transformer/dragging?])]
-    (rx/rx
-     (when (and @loaded? (not @dragging?))
-       (let [current-state @state]
-         (when-let [t @debounce-timeout]
-           (js/clearTimeout t))
-         (reset! debounce-timeout
-                 (js/setTimeout #(write-fragment! current-state) 1000)))))))
+(defn update-fragment
+  [{:keys [db sub]}]
+  (rx/rx (when (and (get @db :loaded?) (not @(sub [:transformer/dragging?])))
+           (let [updater #(if @*navigating?
+                            (reset! *navigating? false)
+                            (write-fragment! (select-keys @db serialize-keys)))]
+             (when-let [t @*debounce-timeout] (js/clearTimeout t))
+             (reset! *debounce-timeout (js/setTimeout updater 250))))))
 
 (def spec {:share/update-fragment update-fragment})
